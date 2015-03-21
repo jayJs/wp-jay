1108816e8360dc9f667d9b46884471ff238adbba
Martin Sookael <m@451.ee>
2015-03-22 01:32:30 +0200

    removed livereload

--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@
 
 $(document).ready(function() {
 
-  // connect-livereload via Gulp autorefreshes the site.
-  $("body").append('<script src="http://localhost:35729/livereload.js?snipver=1"></script>');
-
   // hide loadin + show app
   $("#loading").addClass("animated fadeOut").addClass("hidden");
   $("#app").removeClass("hidden").addClass("animated fadeIn");
