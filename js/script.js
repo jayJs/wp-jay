8dcc01b3cf654bebe625572f49c876a2522fbf8d
Martin Sookael <m@451.ee>
2015-06-17 18:25:10 +0300

    updated script to be compatible with Jay v1.0

--- js/script.js
+++ js/script.js
@@ -1,36 +1,39 @@
-"use strict";
+/*jslint indent: 2*/
+/*global window, console, document, $*/
 
-$(document).ready(function() {
+$(document).ready(function () {
+
+  "use strict";
 
   // hide loadin + show app
   $("#loading").addClass("animated fadeOut").addClass("hidden");
   $("#app").removeClass("hidden").addClass("animated fadeIn");
 
   // get bloginfo and add it to header
-  bloginfo().then(function(data){
+  bloginfo().then(function (data) {
     var d = data;
-    blogName.empty().append(d.name);
-    blogDescription.empty().append(d.description);
-  })
+    $(blogName).empty().append(d.name);
+    $(blogDescription).empty().append(d.description);
+  });
 
-  function clearApp(){
-    onePost.empty().out()
-    listPosts.empty().out()
-    e404.empty().out()
-    $('html,body').scrollTop(0)
+  function clearApp() {
+    $(onePost).empty().hide();
+    $(listPosts).empty().hide();
+    $(e404).empty().hide();
+    $('html,body').scrollTop(0);
   }
 
   // VIEWS - Front page view
   var listPostsView = function () {
     clearApp();
-    listPosts.in('fadeIn');
+    $(listPosts).show('fadeIn');
     listPostsFunction();
   }
 
   // One post view
   var onePostView = function (id) {
     clearApp();
-    onePost.in('fadeIn');
+    $(onePost).show('fadeIn');
     onePostFunction(id);
   }
 
@@ -39,37 +42,37 @@ $(document).ready(function() {
   crossroads.addRoute('/{id}', onePostView);
 
   // Error 404
-  crossroads.bypassed.add(function(request){
+  crossroads.bypassed.add(function (request) {
     clearApp()
     e404.append("404")
   })
 
   // start routing
-  route(crossroads);
+  J.route(crossroads);
 
   // CONTROLLERS, "/"
   function listPostsFunction() {
-    get_posts().then(function(data){
+    get_posts().then(function (data) {
       var d = data
-      listPosts.empty()
+      $(listPosts).empty()
       for (var i = 0; i < d.length; i++) {
-        listPosts.append("<a href='#/"+d[i].slug+"'><h3>"+ d[i].title +"</h3></a>");
-        listPosts.append("<p>"+ d[i].excerpt +"</p>");
+        $(listPosts).append("<a href='#/"+d[i].slug+"'><h3>"+ d[i].title +"</h3></a>");
+        $(listPosts).append("<p>"+ d[i].excerpt +"</p>");
       }
     })
   }
 
   // Controller, "/{id}"
   function onePostFunction(id) {
-    get_post(id).then(function(data){
+    get_post(id).then(function (data) {
       if(data.length != 0) {
         var d = data[0]
-        onePost.empty()
-        onePost.append("<h2>"+d.title+"</h2>")
-        onePost.append("<p>"+d.content+"</p>")
+        $(onePost).empty()
+        $(onePost).append("<h2>"+d.title+"</h2>")
+        $(onePost).append("<p>"+d.content+"</p>")
       } else {
         clearApp()
-        e404.in()
+        $(e404).in()
       }
     })
   }
